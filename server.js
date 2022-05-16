81589e4a399750d8384a9f3640d918a45eee2a52
Jayash Tripathy <jayashtripathy371@gmail.com>
2022-05-17 00:33:28 +0530

    removed some unwanted comments

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.use(express.static('build'));
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-// app.use((req, res, next) => {
-//    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// })
+
 
 const userSocketMap = {};
 function getAllConnectedClients(roomId){
